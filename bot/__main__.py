adc5deb2c421a4dac9ecc2927ddd8e4b7c8c376f
omniumx <92170278+omniumx@users.noreply.github.com>
2021-10-09 17:42:05 +0800

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -41,7 +41,7 @@ def stats(update, context):
             f'<b>├  🎮 RAM : {memory}%</b>\n' \
             f'<b>├  💽 DISK : {disk}%</b>\n' \
             f'<b>│</b>\n' \
-            f'<b>╰──「 🚸 @holicxslavee 🚸 」</b>'
+            f'<b>╰──「https://t.me/wanpissu」</b>'
     sendMessage(stats, context.bot, update)
 
 

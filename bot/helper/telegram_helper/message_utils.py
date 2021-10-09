5ac2f995a15775567a5b02fd58490b582c59b4c9
omniumx <92170278+omniumx@users.noreply.github.com>
2021-10-09 21:14:19 +0800

    Update message_utils.py

--- bot/helper/telegram_helper/message_utils.py
+++ bot/helper/telegram_helper/message_utils.py
@@ -68,7 +68,7 @@ def delete_all_messages():
 
 def update_all_messages():
     msg = get_readable_message()
-    msg += f"<b>BOT UPTIME :</b> <b>{get_readable_time((time.time() - botStartTime))}</b>\n\n" \
+    msg += f"<b>BOT UPTIME 🟢 :</b> <b>{get_readable_time((time.time() - botStartTime))}</b>\n\n" \
            f"<b>CPU : {psutil.cpu_percent()}%</b>" \
            f" <b>RAM : {psutil.virtual_memory().percent}%</b>" \
            f" <b>DISK : {psutil.disk_usage('/').percent}%</b>"
@@ -89,7 +89,7 @@ def update_all_messages():
                     uldl_bytes += float(speedy.split('M')[0]) * 1048576
         dlspeed = get_readable_file_size(dlspeed_bytes)
         ulspeed = get_readable_file_size(uldl_bytes)
-        msg += f"\n<b>DOWN :</b> <b>{dlspeed}ps||</b> <b>UP :</b> <b>{ulspeed}ps</b>"
+        msg += f"\n<b>DOWN :</b> <b>{dlspeed}ps</b> 🌝 <b>||</b> <b>UP :</b> <b>{ulspeed}ps</b> 🌚\n"
     with status_reply_dict_lock:
         for chat_id in list(status_reply_dict.keys()):
             if status_reply_dict[chat_id] and msg != status_reply_dict[chat_id].text:
@@ -105,6 +105,7 @@ def update_all_messages():
 
 def sendStatusMessage(msg, bot):
     progress = get_readable_message()
+    progress += f"<b>BOT UPTIME :</b> <b>{get_readable_time((time.time() - botStartTime))}</b>\n\n" \
            f"<b>CPU : {psutil.cpu_percent()}%</b>" \
            f" <b>RAM : {psutil.virtual_memory().percent}%</b>" \
            f" <b>DISK : {psutil.disk_usage('/').percent}%</b>"
@@ -125,7 +126,7 @@ def sendStatusMessage(msg, bot):
                     uldl_bytes += float(speedy.split('M')[0]) * 1048576
         dlspeed = get_readable_file_size(dlspeed_bytes)
         ulspeed = get_readable_file_size(uldl_bytes)
-        progress += f"\n<b>DOWN :</b> <b>{dlspeed}ps</b><b>||</b> <b>UP :</b> <b>{ulspeed}ps</b>\n"
+        progress += f"\n<b>DOWN :</b> <b>{dlspeed}ps</b> 🌝 <b>||</b> <b>UP :</b> <b>{ulspeed}ps</b> 🌚\n"
     with status_reply_dict_lock:
         if msg.message.chat.id in list(status_reply_dict.keys()):
             try:

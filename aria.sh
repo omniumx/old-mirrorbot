643327f9b4cc0f5c27555c66b28157f51911554b
omniumx <92170278+omniumx@users.noreply.github.com>
2021-10-10 08:47:12 +0800

    Update aria.sh

--- aria.sh
+++ aria.sh
@@ -4,7 +4,7 @@ export MAX_CONCURRENT_DOWNLOADS=8
 
 aria2c --enable-rpc --rpc-listen-all=false --check-certificate=false \
    --max-connection-per-server=10 --rpc-max-request-size=1024M \
-   --bt-tracker="[$tracker_list]" --bt-max-peers=50 --bt-tracker-connect-timeout=300 --bt-stop-timeout=1200 --min-split-size=10M \
+   --bt-tracker="[$tracker_list]" --bt-max-peers=40 --bt-tracker-connect-timeout=300 --bt-stop-timeout=1200 --min-split-size=10M \
    --follow-torrent=mem --split=10 \
    --daemon=true --allow-overwrite=true --max-overall-download-limit=$MAX_DOWNLOAD_SPEED \
    --max-overall-upload-limit=1K --max-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS \
